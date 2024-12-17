b242e53faf247a7b7a1fb30f92154ff8f7256811
esthermdev <esmd258@gmail.com>
2024-12-17 21:19:51 +0800

    update: Fetched pools for division standings

--- app/(tabs)/standings/[division].tsx
+++ app/(tabs)/standings/[division].tsx
@@ -1,23 +1,35 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, ActivityIndicator, Text } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
+import { usePoolIds } from '@/hooks/useGamesFilter';
 
+export default function DivisionStandings() {
+  const { division } = useLocalSearchParams();
+  const { pools, loading, error } = usePoolIds(Number(division));
 
-const DivisionStandings = () => {
-  const { division, divisionName } = useLocalSearchParams();
-
+  if (loading) {
     return (
-        <View style={styles.container}>
-          <Text>{divisionName} Standings</Text>
-        </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#EA1D25" />
+      </View>
     );
-};
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-});
+  if (error || !pools.length) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>No pools found</Text>
+      </View>
+    );
+  }
 
-export default DivisionStandings;
+  return (
+    <View>
+      {pools.map((pool, index) => (
+        <View key={index}>
+          <Text>{pool.name}</Text>
+        </View>
+      ))}
+    </View>
+  );
+}
