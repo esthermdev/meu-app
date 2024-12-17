c11199c7228e711d977aca79989551c2a19e0d14
esthermdev <esmd258@gmail.com>
2024-12-17 21:38:38 +0800

    feat: Added fetchStandings function to fetch standings data

--- app/(tabs)/standings/[division].tsx
+++ app/(tabs)/standings/[division].tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import { useState } from 'react';
 import { View, ActivityIndicator, Text } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import { usePoolIds } from '@/hooks/useGamesFilter';
+import { supabase } from '@/lib/supabase';
+import { Database } from '@/database.types';
+
+type StandingsRow = Database['public']['Tables']['rankings']['Row']
+type TeamsRow = Database['public']['Tables']['teams']['Row'];
+
+interface Standings extends StandingsRow {
+  teams: TeamsRow | null;
+}
 
 export default function DivisionStandings() {
   const { division } = useLocalSearchParams();
   const { pools, loading, error } = usePoolIds(Number(division));
+  const [standings, setStandings] = useState<Standings[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchStandings = async () => {
+    setIsLoading(true);
+
+    const poolIds = pools.map(pool => pool.id)
+
+    const { data, error } = await supabase
+      .from('rankings')
+      .select(`
+        wins,
+        losses,
+        pool_rank,
+        teams!inner (pool_id, seed, name)
+      `)
+      .in('teams.pool_id', poolIds)
+      .order('teams(pool_id)', { ascending: true })
+      .order('pool_rank', { ascending: true });
+
+      if (error) {
+          console.error('Error fetching teams:', error);
+      } else {
+        setStandings(data as Standings[])
+      }
+    
+    setIsLoading(false);
+  };
 
   if (loading) {
     return (
