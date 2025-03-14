4ef5bef99d5cc697cc9ea07318eb230fc3c2f81a
Esther Devadas <esmd258@gmail.com>
2025-03-14 09:05:21 -0400

    Created constants for images and typography

---
+++ constants/Typography.ts
@@ -0,0 +1,72 @@
+// constants/Typography.ts
+export const fonts = {
+  thin: 'GeistThin',
+  extraLight: 'GeistExtraLight', 
+  light: 'GeistLight',
+  regular: 'GeistRegular',
+  medium: 'GeistMedium',
+  semiBold: 'GeistSemiBold',
+  bold: 'GeistBold',
+  extraBold: 'GeistExtraBold',
+  black: 'GeistBlack',
+};
+
+export const typography = {
+  // Headings
+  h1: {
+    fontFamily: fonts.bold,
+    fontSize: 32,
+  },
+  h2: {
+    fontFamily: fonts.bold,
+    fontSize: 28,
+  },
+  h3: {
+    fontFamily: fonts.bold,
+    fontSize: 24,
+  },
+  h4: {
+    fontFamily: fonts.semiBold,
+    fontSize: 20,
+  },
+  h5: {
+    fontFamily: fonts.medium,
+    fontSize: 18,
+  },
+  
+  // Body text
+  bodyLarge: {
+    fontFamily: fonts.bold,
+    fontSize: 18,
+  },
+  bodyMedium: {
+    fontFamily: fonts.semiBold,
+    fontSize: 16,
+  },
+  body: {
+    fontFamily: fonts.regular,
+    fontSize: 14,
+  },
+  bodyBold: {
+    fontFamily: fonts.bold,
+    fontSize: 14,
+  },
+  bodySmall: {
+    fontFamily: fonts.regular,
+    fontSize: 12,
+  },
+  
+  // Other styles
+  button: {
+    fontFamily: fonts.bold,
+    fontSize: 18,
+  },
+  caption: {
+    fontFamily: fonts.regular,
+    fontSize: 12,
+  },
+  label: {
+    fontFamily: fonts.bold,
+    fontSize: 16,
+  },
+};
