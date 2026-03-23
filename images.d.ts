f157c035d4e995b618d56cfe6e51938f97bfe1ce
Esther Devadas <esmd258@gmail.com>
2026-03-22 20:30:59 -0400

    chore: update dependencies in package.json

--- images.d.ts
+++ images.d.ts
@@ -3,4 +3,11 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.gif';
-declare module '*.svg';
+
+declare module '*.svg' {
+  import type { FC } from 'react';
+  import type { SvgProps } from 'react-native-svg';
+
+  const content: FC<SvgProps>;
+  export default content;
+}
--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,18 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 const wsShimPath = path.resolve(__dirname, 'shims/ws.js');
+const { transformer, resolver } = config;
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+};
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg'],
+};
 
 config.resolver.extraNodeModules = {
   ...(config.resolver.extraNodeModules || {}),
